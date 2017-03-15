0a49ac4f33595dd7ab60090550a023ad278b1418
Kelvin Kong <kcw88225@gmail.com>
2017-03-15 22:41:51 +0800

    add column annotation

--- src/main/java/com/in28minutes/dto/UserDetails.java
+++ src/main/java/com/in28minutes/dto/UserDetails.java
@@ -1,12 +1,15 @@
 package com.in28minutes.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name="USER_DETAILS")
 public class UserDetails {
 	@Id
+	@Column(name="USER_ID")
 	private int userId;
+	@Column(name="USER_NAME")
 	private String name;
 	
 	public int getUserId() {
